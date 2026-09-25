830ac5422a4820f80f7cec756a226860f21839e5
Dhruv Patel <dhruvsutariya793@gmail.com>
2026-09-25 10:04:06 +0530

    Second commit

---
+++ MLPROJECT/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages,setup
+from typing import List
+
+H_E_D = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    return list of requirements
+    '''
+    requirements=[]
+
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n',"") for req in requirements]
+
+        if H_E_D in requirements:
+            requirements.remove(H_E_D)
+    return requirements
+
+        
+
+
+
+setup(
+    name='mlproject',
+    version='0.0.2',
+    author='Dhruv',
+    author_email='dhruvsutariya793@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
