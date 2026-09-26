e69195d10963e16260105426b7a888ccd2c9caa7
Dhruv Patel <dhruvsutariya793@gmail.com>
2026-09-26 14:09:38 +0530

    sixth commit

--- MLPROJECT/app.py
+++ MLPROJECT/app.py
@@ -1,5 +1,7 @@
 from src.ML_Project.logger import logging
 from src.ML_Project.exception import CustomException
+from src.ML_Project.components.data_ingestion import DataIngestion
+from src.ML_Project.components.data_ingestion import DataIngestionConfig
 import sys
 
 
@@ -7,7 +9,9 @@ if __name__ == '__main__':
     logging.info("Execution Started")
     
     try:
-        x = 1/0
+        # data_ingestion_config=DataIngestionConfig
+        data_ingestion=DataIngestion()
+        data_ingestion.initiate_data_ingestion()
     except Exception as e:
         logging.info("Custome Exception")
         raise CustomException(e,sys)
